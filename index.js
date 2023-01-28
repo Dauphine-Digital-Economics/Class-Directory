e228d19c5ee90d5c666a2cdc68031fd7c8ae098d
TC <tianchansa@gmail.com>
2023-01-28 14:07:07 +0100

    change to new function calls

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 window.onload = function(){
 	document.getElementById("balanceBtn").onclick = async() => {
-		if (typeof web3 !== 'undefined') {
-		    console.log('web3 is enabled')
-		    const web3 = await logIntoMetaMask();
-		    const currentWallet = await web3.eth.requestAccount()[0];
-		    console.log(currentWallet);
+		if (typeof window.ethereum !== 'undefined') {
+		    console.log('web3 is enabled');
+		    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+			const currentAccount = accounts[0];
+			console.log(currentAccount);
 		} else{
 			alert("Install MetaMask or connect a provider");
 		}
 
 	};
-
-	function logIntoMetaMask(){
-		return new Promise(async (resolve, reject)=> {
-			var web3 = new Web3(window.ethereum);	
-			try{
-			    await window.ethereum.request({method : "eth_accounts"});
-			    resolve(web3);
-			} catch (e) {
-				console.log(e);
-			    reject(error);
-			}
-		})
-	}
 };
