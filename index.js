dc7e7be651164628f39dfb0550f28a202a08875c
TC <tianchansa@gmail.com>
2023-01-28 12:56:39 +0100

    update metamask getaccount

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ window.onload = function(){
 		console.log("clicked");
 		if (typeof web3 !== 'undefined') {
 		    console.log('web3 is enabled')
+		    // Instantiate a web3 object
+		    var web3 = new Web3(window.ethereum);
+
 		    if (web3.currentProvider.isMetaMask === true) {
 		      console.log('MetaMask is active')
+		      	try{
+		      		await window.ethereum.enable();
+		      		return true;
+		      	} catch (e) {
+		      		alert(e);
+		      		return false;
+		      	}
+
+		      	var accounts = await web3.eth.getAccounts().then(console.log);
+		    	
 		    } else {
-		      console.log('MetaMask is not available')
+		    	// Popup to ask user to install metamask
+		    	alert("Please install Metamask on your current browser");
 		    }
 		} else {
-		    console.log('web3 is not found')
+			// No web3 instance
+			alert("Please install Metamask or connect a web3 Provider");
 		}
 	};
 };
