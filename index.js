3b2a0e2892bc610c0ff821201dd7dbaf3d3c49c1
TC <tianchansa@gmail.com>
2023-01-28 22:44:25 +0100

    change to native ethereum RPC

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ window.onload = function(){
 		    console.log('web3 is enabled');
 		    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
 			const currentAccount = accounts[0];
-			console.log(currentAccount);
+			await ethereum.request({method: 'eth_getBalance', params: [currentAccount, 'latest']}).then(
+				(balance) => {
+					alert("Success! Your current Balance is" + balance);
+				}).catch(() => {
+					alert("getBalance RPC call failed");
+				});
+			console.log(balance);
 		} else{
 			alert("Install MetaMask or connect a provider");
 		}
