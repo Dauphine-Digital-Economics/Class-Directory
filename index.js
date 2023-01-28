e68362f145ba23e4c0af8a7c73c251a1fd46a6df
TC <tianchansa@gmail.com>
2023-01-28 22:58:16 +0100

    final cleanup

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 window.onload = function(){
 	document.getElementById("balanceBtn").onclick = async() => {
 		if (typeof window.ethereum !== 'undefined') {
-		    console.log('web3 is enabled');
 		    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
 			const currentAccount = accounts[0];
 			await ethereum.request({method: 'eth_getBalance', params: [currentAccount, 'latest']}).then(
-				(balance) => {
-					const decBalance = HexToWei(balance);
+				(value) => {
+					const decBalance = HexToWei(value);
 					alert("Success! Your current Balance is " + decBalance + " Wei");
-				}).catch(() => {
-					alert("getBalance RPC call failed");
+				}, (reason) => {
+					alert("getBalance failed due to " + reason);
 				});
 		} else{
 			alert("Install MetaMask or connect a provider");
 		}
-
 	};
 
 	function HexToWei(hexBalance){
