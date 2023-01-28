a6a5f7a146540e29184649843cbf5a65be5b7134
TC <tianchansa@gmail.com>
2023-01-28 13:24:23 +0100

    add async

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 window.onload = function(){
 	document.getElementById("balanceBtn").onclick = function(){
-		console.log("clicked");
 		if (typeof web3 !== 'undefined') {
 		    console.log('web3 is enabled')
+
 		    // Instantiate a web3 object
 		    var web3 = new Web3(window.ethereum);
 
 		    if (web3.currentProvider.isMetaMask === true) {
 		      console.log('MetaMask is active')
 		      	try{
+		      		console.log("enter try statement")
 		      		await window.ethereum.enable();
 		      		return true;
 		      	} catch (e) {
+		      		console.log("error checking if metamask is logged in")
 		      		alert(e);
 		      		return false;
 		      	}
-
-		      	var accounts = await web3.eth.getAccounts().then(console.log);
+		      	console.log("get accounts")
+		      	const accounts = await web3.eth.getAccounts().then(console.log(accounts.length));
 		    	
 		    } else {
 		    	// Popup to ask user to install metamask
